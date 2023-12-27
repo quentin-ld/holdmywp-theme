e361e01453e586c348138549fd49136fa94b6d73
Quentin LD <quentin.ld@icloud.com>
2023-12-27 19:44:18 +0100

    initial theme commit

---
+++ patterns/404.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Title: 404
+ * Slug: Hold my WP/404
+ * Categories: hidden
+ * Inserter: no
+ */
+?>
+<!-- wp:template-part {"slug":"header","area":"header"} /-->
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:heading {"textAlign":"center","level":1,"style":{"typography":{"fontSize":"85px","fontStyle":"normal","fontWeight":"900","lineHeight":"1"},"spacing":{"margin":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}}}} -->
+<h1 class="wp-block-heading has-text-align-center" style="margin-top:0px;margin-right:0px;margin-bottom:0px;margin-left:0px;font-size:85px;font-style:normal;font-weight:900;line-height:1">404</h1>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center","style":{"typography":{"lineHeight":"1"}},"textColor":"custom-accent-1"} -->
+<p class="has-text-align-center has-custom-accent-1-color has-text-color" style="line-height:1"><strong>tu t'es perdu ?</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:image {"align":"center","id":662,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image aligncenter size-full"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/boromir.gif" alt="" class="wp-image-662"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"textAlign":"left"} -->
+<div class="wp-block-button"><a class="wp-block-button__link has-text-align-left wp-element-button" href="#">😬 Revenir sur la page d'accueil</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group -->
+
+<!-- wp:template-part {"slug":"footer","tagName":"footer"} /-->
---
+++ patterns/footer.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Title: footer
+ * Slug: Hold my WP/footer
+ * Categories: hidden
+ * Inserter: no
+ */
+?>
+<!-- wp:group {"style":{"spacing":{"margin":{"top":"80px","bottom":"0px"}}},"layout":{"inherit":true,"type":"constrained"}} -->
+<div class="wp-block-group" style="margin-top:80px;margin-bottom:0px"><!-- wp:group {"style":{"spacing":{"padding":{"top":"80px","bottom":"30px"}},"border":{"top":{"color":"var:preset|color|custom-accent-1","width":"2px"},"right":[],"bottom":[],"left":[]}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+<div class="wp-block-group" style="border-top-color:var(--wp--preset--color--custom-accent-1);border-top-width:2px;padding-top:80px;padding-bottom:30px"><!-- wp:paragraph {"align":"center","fontSize":"medium"} -->
+<p class="has-text-align-center has-medium-font-size"><a href="#" data-type="page" data-id="443"><strong>Navigation</strong></a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center"><a href="#" data-type="page" data-id="73">Mentions légales</a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center"><a href="#" data-type="page" data-id="17">Politique de confidentialité</a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><a href="#" data-type="link" data-id="#" target="_blank" rel="noreferrer noopener">Plan du site XML ↗️</a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"40px","bottom":"40px"}}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+<div class="wp-block-group" style="padding-top:40px;padding-bottom:40px"><!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center">☝️<br><a href="#top" data-type="internal" data-id="#top">Retour en haut de la page</a> </p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"center"}} -->
+<div class="wp-block-group"><!-- wp:image {"id":1093,"width":"32px","height":"auto","scale":"contain","sizeSlug":"thumbnail","linkDestination":"custom","className":"drop-shadow-md"} -->
+<figure class="wp-block-image size-thumbnail is-resized drop-shadow-md"><a href="#"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/holdmywp-logo-2-150x150.png" alt="" class="wp-image-1093" style="object-fit:contain;width:32px;height:auto"/></a></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph {"align":"center","fontSize":"small"} -->
+<p class="has-text-align-center has-small-font-size"><em>Développé par <a href="#" data-type="link" data-id="#">Quentin Le Duff</a> - <em> Propulsé par <a href="https://wordpress.org/" data-type="link" data-id="https://wordpress.org/" target="_blank" rel="noreferrer noopener">WordPress</a> & <a href="https://wordpress.org/gutenberg/" data-type="link" data-id="https://wordpress.org/gutenberg/" target="_blank" rel="noreferrer noopener">Gutenberg</a></em></em></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
