1f46bc6061cf3b0fe21c423dcce34548f64a95dd
Quentin LD <quentin.ld@icloud.com>
2024-01-21 09:00:48 +0100

    feat(theme): update parts

--- patterns/footer.php
+++ patterns/footer.php
@@ -9,19 +9,19 @@
 <!-- wp:group {"style":{"spacing":{"margin":{"top":"80px","bottom":"0px"}}},"layout":{"inherit":true,"type":"constrained"}} -->
 <div class="wp-block-group" style="margin-top:80px;margin-bottom:0px"><!-- wp:group {"style":{"spacing":{"padding":{"top":"80px","bottom":"30px"}},"border":{"top":{"color":"var:preset|color|custom-accent-1","width":"2px"},"right":[],"bottom":[],"left":[]}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
 <div class="wp-block-group" style="border-top-color:var(--wp--preset--color--custom-accent-1);border-top-width:2px;padding-top:80px;padding-bottom:30px"><!-- wp:paragraph {"align":"center","fontSize":"medium"} -->
-<p class="has-text-align-center has-medium-font-size"><a href="#" data-type="page" data-id="443"><strong>Navigation</strong></a></p>
+<p class="has-text-align-center has-medium-font-size"><a href="https://holdmywp.com/navigation/" data-type="page" data-id="443"><strong>Navigation</strong></a></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:paragraph {"align":"center"} -->
-<p class="has-text-align-center"><a href="#" data-type="page" data-id="73">Mentions légales</a></p>
+<p class="has-text-align-center"><a href="https://holdmywp.com/mentions-legales/" data-type="page" data-id="73">Mentions légales</a></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:paragraph {"align":"center"} -->
-<p class="has-text-align-center"><a href="#" data-type="page" data-id="17">Politique de confidentialité</a></p>
+<p class="has-text-align-center"><a href="https://holdmywp.com/politique-de-confidentialite/" data-type="page" data-id="17">Politique de confidentialité</a></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:paragraph -->
-<p><a href="#" data-type="link" data-id="#" target="_blank" rel="noreferrer noopener">Plan du site XML ↗️</a></p>
+<p><a href="https://holdmywp.com/sitemap_index.xml" data-type="link" data-id="https://holdmywp.com/sitemap_index.xml" target="_blank" rel="noreferrer noopener">Plan du site XML&nbsp;↗️</a></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:group {"style":{"spacing":{"padding":{"top":"40px","bottom":"40px"}}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
@@ -32,11 +32,11 @@
 
 <!-- wp:group {"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"center"}} -->
 <div class="wp-block-group"><!-- wp:image {"id":1093,"width":"32px","height":"auto","scale":"contain","sizeSlug":"thumbnail","linkDestination":"custom","className":"drop-shadow-md"} -->
-<figure class="wp-block-image size-thumbnail is-resized drop-shadow-md"><a href="#"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/holdmywp-logo.png" alt="" class="wp-image-1093" style="object-fit:contain;width:32px;height:auto"/></a></figure>
+<figure class="wp-block-image size-thumbnail is-resized drop-shadow-md"><a href="https://holdmywp.com/"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/holdmywp-logo.png" alt="" class="wp-image-1093" style="object-fit:contain;width:32px;height:auto"/></a></figure>
 <!-- /wp:image -->
 
 <!-- wp:paragraph {"align":"center","fontSize":"small"} -->
-<p class="has-text-align-center has-small-font-size"><em>Développé par <a href="#" data-type="link" data-id="#">Quentin Le Duff</a> - <em> Propulsé par <a href="https://wordpress.org/" data-type="link" data-id="https://wordpress.org/" target="_blank" rel="noreferrer noopener">WordPress</a> & <a href="https://wordpress.org/gutenberg/" data-type="link" data-id="https://wordpress.org/gutenberg/" target="_blank" rel="noreferrer noopener">Gutenberg</a></em></em></p>
+<p class="has-text-align-center has-small-font-size"><em>Développé par <a href="https://holdmywp.com/" data-type="page" data-id="431">Quentin Le Duff</a> - <em> Propulsé par <a href="https://wordpress.org/" data-type="link" data-id="https://wordpress.org/" target="_blank" rel="noreferrer noopener">WordPress</a> & <a href="https://wordpress.org/gutenberg/" data-type="link" data-id="https://wordpress.org/gutenberg/" target="_blank" rel="noreferrer noopener">Gutenberg</a></em></em></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group --></div>
